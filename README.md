eaa225e57213e4a226481a305ba279d10a7d35cc
haowang <hw26@rice.edu>
2019-02-12 22:32:49 -0600

    m

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# tests
+# Weather Report
+
+A simple command line tool that asks the user for the city
+and prints the current tenmprature of the city in fahrenheit
--- dwolla_technical-test.py
+++ dwolla_technical-test.py
@@ -1,33 +1,45 @@
-import unittest,sys
+import unittest,sys,urllib
 from dwolla_technical import main
 
 
-
+def raise_HTTPError():
+    raise urllib.error.HTTPError(e.url, e.code)
+def raise_URLError():
+    raise urllib.error.URLError(e.url, e.code)    
 
 class TestAnswerReturn(unittest.TestCase):
     def testHouston(self):
     	__builtins__.raw_input = lambda _: 'Houston'
         main()
         output = sys.stdout.getvalue().strip()
-        self.assertEqual(output,'Houston temprature is 43 fahrenheit')
+        deg = float(output.split(" ")[3])
+        self.assertAlmostEqual(deg,38.0,delta = 2)
 
     def testSanFrancisco(self):
     	__builtins__.raw_input = lambda _:"San Francisco"
         main()
         output = sys.stdout.getvalue().strip()
-        self.assertEqual(output, 'San Francisco temprature is 36 fahrenheit')
+        deg = float(output.split(" ")[4])
+        self.assertAlmostEqual(deg,36.0,delta = 2)
 
     def testAtlanta(self):
     	__builtins__.raw_input = lambda _:"Atlanta"
         main()
         output = sys.stdout.getvalue().strip()
-        self.assertEqual(output,'Atlanta temprature is 37 fahrenheit')
+        deg = float(output.split(" ")[3])
+        self.assertAlmostEqual(deg,36.0,delta = 2)
 
     def testNoCity(self):
     	__builtins__.raw_input = lambda _:"xxx"
         main()
         output = sys.stdout.getvalue().strip()
         self.assertEqual(output, 'city not found')
+
+
+
+
+    
+
 
 
 
